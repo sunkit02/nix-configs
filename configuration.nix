674eb4623e8fdfb936f027ff840b39d50add0f16
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-05-12 23:00:33 -0500

    add home-manager and flakes

--- configuration.nix
+++ configuration.nix
@@ -107,6 +107,9 @@
 #  wget
   ];
 
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
---
+++ home.nix
@@ -0,0 +1,30 @@
+{ ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "sunkit";
+  home.homeDirectory = "/home/sunkit";
+
+  # Packages that should be installed to the user profile.
+  home.packages = [ ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "sunkit02";
+    userEmail = "sunkit02@users.noreply.github.com";
+  };
+}
